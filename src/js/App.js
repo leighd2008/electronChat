8fffd691c8ac6b2aae34faeda01b022340721a10
Diane Leigh <leighd2008@gmail.com>
2023-02-06 14:03:38 -0500

    create App.js file

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  const title = "Hello World My Perfect Application";
+  const enhancedTitle = title + ' - React App!';
+  
+  return (
+    <h1>{enhancedTitle}</h1>
+  )
+}
+
+export default App;
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 
 
 import React from 'react';
+import App from './App';
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('electronChat');
 const root = createRoot(container);
-root.render(<h1>I am React App!</h1>);
+root.render(<App />);
