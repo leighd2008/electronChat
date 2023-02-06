6dee39247b4b4e4180eeb65c8f9c55fa1a48f76d
Diane Leigh <leighd2008@gmail.com>
2023-02-06 11:35:22 -0500

    initial electron browser window implemented

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
+// Main Process
+const { app, BrowserWindow } = require('electron');
 
+// Renderer Process
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: "lavender",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  
+  win.loadFile('index.html')
+  win.webContents.openDevTools();
+}
 
+app.whenReady().then(createWindow);
 
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
 
-
-console.log('Hello World');
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
