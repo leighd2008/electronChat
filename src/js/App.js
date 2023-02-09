ff7b2ab2c7ba5925b9ca7f04c5139b61b2aa29bd
Diane Leigh <leighd2008@gmail.com>
2023-02-09 12:00:45 -0500

    added button for notification

--- src/js/App.js
+++ src/js/App.js
@@ -4,8 +4,15 @@ const App = () => {
   const title = "Hello World";
   const enhancedTitle = title + ' - React App!';
   
+  const sendNotification = () => {
+    alert('Hello World');
+  }
+  
   return (
-    <h1>{enhancedTitle}</h1>
+    <>
+      <h1>{enhancedTitle}</h1>
+      <button onClick={sendNotification}>Send Notification</button>
+    </>
   )
 }
 
