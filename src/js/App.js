3ed01aee3213c4299ea7a5525976b7eae00f223d
Diane Leigh <leighd2008@gmail.com>
2023-02-12 12:03:28 -0500

    implemented router

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
 import HomeView from '../views/Home';
+import Navbar from '../components/Navbar'
+
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   
-    // ########## HOME VIEW START ############
     return (
-      <HomeView />
+      <Router>
+        <Navbar />
+        <div className='content-wrapper'>
+          <Routes>
+            <Route path='/settings' element={<h1>I am settings view</h1>} />
+            <Route path='/login' element={<h1>I am login view</h1>} />
+            <Route path='/register' element={<h1>I am register view</h1>} />
+            <Route path='/' element={<HomeView />} />
+          </Routes>
+        </div>
+      </Router>
     )
-    // ########## HOME VIEW END ############
   }
 
 export default App;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,6 @@
 
 
 import React from "react";
-import Navbar from "../components/Navbar";
 import JoinedChats from "../components/JoinedChats";
 import AvailableChats from "../components/AvailableChats";
 import ViewTitle from "../components/shared/ViewTitle";
@@ -9,16 +8,13 @@ import ViewTitle from "../components/shared/ViewTitle";
 
 export default function Home() {
   return (
-    <div className='content-wrapper'>
-      <Navbar />
-      <div className="row no-gutters fh">
-        <div className="col-3 fh">
-          <JoinedChats />
-        </div>
-        <div className="col-9 fh">
-          <ViewTitle />
-          <AvailableChats />
-        </div>
+    <div className="row no-gutters fh">
+      <div className="col-3 fh">
+        <JoinedChats />
+      </div>
+      <div className="col-9 fh">
+        <ViewTitle />
+        <AvailableChats />
       </div>
     </div>
   )
