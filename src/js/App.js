4fa04527e383a08b656b12b82bc9e7199d3458bb
Diane Leigh <leighd2008@gmail.com>
2023-02-19 10:22:11 -0500

    initiated redux store, did not transition from redux createStore to redux-toolkit configureStore, will do this after writing reducers

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { Provider } from 'react-redux';
+
 import HomeView from '../views/Home';
 import ChatView from '../views/Chat';
 import LoginView from '../views/Login';
@@ -7,24 +10,29 @@ import RegisterView from '../views/Register';
 
 
 import Navbar from '../js/components/Navbar'
+import configureStore from '../js/store/index';
+
+const store = configureStore();
 
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   
     return (
-      <Router>
-        <Navbar />
-        <div className='content-wrapper'>
-          <Routes>
-            <Route path='/' exact element={<HomeView />} />
-            <Route path='/chat/:id' element={<ChatView />} />
-            <Route path='/settings' element={<SettingsView />} />
-            <Route path='/login' element={<LoginView />} />
-            <Route path='/register' element={<RegisterView />} />
-          </Routes>
-        </div>
-      </Router>
+      <Provider store={store} >
+        <Router>
+          <Navbar />
+          <div className='content-wrapper'>
+            <Routes>
+              <Route path='/' exact element={<HomeView />} />
+              <Route path='/chat/:id' element={<ChatView />} />
+              <Route path='/settings' element={<SettingsView />} />
+              <Route path='/login' element={<LoginView />} />
+              <Route path='/register' element={<RegisterView />} />
+            </Routes>
+          </div>
+        </Router>
+      </Provider>
     )
   }
 
