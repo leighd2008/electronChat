21c189b6203a6ecd971efded6c176d22ab6392de
Diane Leigh <leighd2008@gmail.com>
2023-02-27 08:29:39 -0500

    retrieve user profile from firebase, usename in navbar

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -21,15 +21,18 @@ export default function Navbar() {
             className="btn btn-outline-success ml-2">Settings</Link>
         </div>
         <div className="chat-navbar-inner-right">
-          <span className="logged-in-user">Hi User</span>
-          <Link
+          {/* <Link
             to='/'
-            className="btn btn-outline-success ml-2">Login</Link>
+            className="btn btn-outline-success ml-2">Login</Link> */}
           { user &&
-            <button
-              onClick={() => dispatch(logout())}
-              className="btn btn-outline-danger ml-2" >Logout
-            </button>
+            <>
+              <img className="avatar mr-2" src={user.avatar} alt="avatar" />
+              <span className="logged-in-user">Hi, {user.username}</span>
+              <button
+                onClick={() => dispatch(logout())}
+                className="btn btn-outline-danger ml-4" >Logout
+              </button>
+            </>
           }
           <button
             onClick={() => navigate(-1)}
