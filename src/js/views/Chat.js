92d6ef2c96c20ec9aa2e9f7ff14cfd097e0f56c7
Diane Leigh <leighd2008@gmail.com>
2023-03-06 09:06:30 -0500

    subscribe to chat, to see changes in real time

--- src/js/views/Chat.js
+++ src/js/views/Chat.js
@@ -1,14 +1,25 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import ChatUserList from "../components/ChatUsersList";
 import ViewTitle from "../components/shared/ViewTitle";
 import ChatMessagesList from "../components/ChatMessagesList";
 import { withBaseLayout } from "../layouts/Base";
 
+import { subscribeToChat } from "../actions/chats";
+
 function Chat () {
   const { id } = useParams();
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    const unsubFromChat = dispatch(subscribeToChat(id));
+    return () => {
+      unsubFromChat();
+    }
+  }, [])
   
   return (
     <div className="row no-gutters fh">
