5fa87ecd0d4c2f570fc0f2680d3dd512ef49d48f
Diane Leigh <leighd2008@gmail.com>
2023-03-06 09:24:28 -0500

    extract user information from the chat

--- src/js/actions/chats.js
+++ src/js/actions/chats.js
@@ -38,7 +38,12 @@ export const createChat = (formData, userId) => async dispatch => {
 }
 
 export const subscribeToChat = chatId => dispatch =>
-  api.subscribeToChat(chatId, (chat) => {
+  api.subscribeToChat(chatId, async (chat) => {
+    
+    const joinedUsers = await Promise.all(chat.joinedUsers.map(async userRef => {
+      const userSnapshot = await userRef.get();
+      return userSnapshot.data()
+    }))
     dispatch({type: 'CHATS_SET_ACTIVE_CHAT', chat})
   })
 
