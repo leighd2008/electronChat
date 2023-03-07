cf5861e5807a9b060e46d8653cd567c4065a09b4
Diane Leigh <leighd2008@gmail.com>
2023-03-07 09:37:42 -0500

    fixing online status after logout

--- src/js/api/connection.js
+++ src/js/api/connection.js
@@ -17,4 +17,7 @@ export const onConnectionChanged = onConnection =>
   firebase
     .database()
     .ref('.info/connected')
-    .on('value', snapshot => onConnection(snapshot.val()))
+    .on('value', snapshot => {
+      const isConnected = snapshot?.val() ? snapshot.val() : false;
+      onConnection(isConnected)
+    })
