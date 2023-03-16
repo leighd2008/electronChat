0ad4dc11428e8c5e19193f4e277951555186e961
Diane Leigh <leighd2008@gmail.com>
2023-03-16 09:17:04 -0400

    UX messasge improvements

--- src/js/components/ChatMessagesList.js
+++ src/js/components/ChatMessagesList.js
@@ -1,14 +1,21 @@
 
-import React from "react";
+import React, { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { formatTimeAgo } from '../utils/time';
 
 export default function ChatMessagesList(messages = []) {
-  console.log(messages)
+  const user = useSelector(({auth}) => auth.user);
+  
+  const isAuthOf = useCallback(message => {
+    return message?.author.uid === user.uid ? 'chat-right' : 'chat-left'
+  })
+  
   return (
     <div className="chat-container">
       <ul className="chat-box chatContainerScroll">
         { messages.messages.map((message, i) =>
           <li key={i}
-            className="chat-left">
+            className={isAuthOf(message)}>
             <div className="chat-avatar">
               <img
                 src={message?.author.avatar}
@@ -18,7 +25,7 @@ export default function ChatMessagesList(messages = []) {
             <div className="chat-text-wrapper">
               <span className="chat-text">{message.content}</span>
               <span className="chat-spacer"></span>
-              <div className="chat-hour">{message.timestamp}</div>
+              <div className="chat-hour">{formatTimeAgo(message.timestamp)}</div>
             </div>
           </li>
         )}
