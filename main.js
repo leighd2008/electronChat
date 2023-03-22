c307c88e69a2188416b6b72347e7d3375bd99198
Diane Leigh <leighd2008@gmail.com>
2023-03-22 10:29:34 -0400

    dock icon for mac

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const { app, BrowserWindow, ipcMain, Notification, Menu } = require('electron');
 const path = require('path');
 const isDev = !app.isPackaged;
 
+const dockIcon = path.join(__dirname, 'assets', 'images', 'react_app_logo.png');
+
 // Renderer Process
 function createWindow() {
   const win = new BrowserWindow({
@@ -40,6 +42,10 @@ if (isDev) {
   require('electron-reload')(__dirname, {
     electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
   })
+}
+
+if (process.platform === 'darwin') {
+  app.dock.setIcon(dockIcon);
 }
 
 app.whenReady()
